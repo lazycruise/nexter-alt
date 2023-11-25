14e3e2b995a3ee8aa7c2da41b139acb50608ab2d
lazycruise <femiakinlua@student.oauife.edu.ng>
2023-11-25 20:02:08 +0100

    add first nexter-alt instance

---
+++ app/api/route.js
@@ -0,0 +1,12 @@
+export const revalidate = 0; // revalidate at most every hour
+
+// Ably Token Authentication API
+import Ably from "ably/promises";
+
+export async function GET(request) {
+  const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: "chat-with-ai",
+  });
+  return Response.json(tokenRequestData);
+}
